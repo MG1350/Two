d8b5db8ff8a6ed280a0ef54ec379c1bd10a08eab
MG1350 <114371390+MG1350@users.noreply.github.com>
2024-09-09 14:55:39 -0400

    Create sumOfCubes.java

---
+++ sumOfCubes.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class sumOfCubes
+{
+    public static void main(String[] args)
+    {
+        double num;
+        double total = 0;
+        double pow = 1;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        num = input.nextInt();
+        input.close();
+        while(num > 0)
+        {
+            double digit = num % (Math.pow(10,pow));
+            total = total + Math.pow(digit / (Math.pow(10, pow-1)),3);
+            System.out.println("Change " + total);
+            pow++;
+            num = num - digit;
+        }
+        System.out.println(total);
+    }
+}
